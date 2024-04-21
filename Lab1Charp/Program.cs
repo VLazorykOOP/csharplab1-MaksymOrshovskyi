2fb7626bc6b54c4e0e5912265e1f23f757970285
Orshovskyi Maksym <137319202+MaksymOrshovskyi@users.noreply.github.com>
2024-04-21 18:27:34 +0300

    task 1

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -1,16 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
- static void task1()
-{
-    Console.WriteLine("Task1 !");
-    Console.Write("s= ");
-    string? str = Console.ReadLine();
-    float s = 0; 
-    if (str != null) s = float.Parse(str);
-    double p = 4 * Math.Sqrt(s);
-    Console.WriteLine("p=" + p);
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        double perimeter;
+
+        // Введення периметру від користувача
+        Console.WriteLine("Введіть периметр трикутника:");
+        while (!double.TryParse(Console.ReadLine(), out perimeter) || perimeter <= 0)
+        {
+            Console.WriteLine("Введіть коректне значення периметру (позитивне число):");
+        }
+
+        // Знаходження довжини сторони рівностороннього трикутника
+        double sideLength = perimeter / 3;
+
+        // Обчислення площі трикутника
+        double area = Math.Sqrt(3) / 4 * sideLength * sideLength;
+
+        // Виведення результату
+        Console.WriteLine("Площа рівностороннього трикутника з периметром {0} дорівнює {1}", perimeter, area);
+    }
 }
-Console.WriteLine("Lab 1 !");
-task1();
+
 // continue ...
